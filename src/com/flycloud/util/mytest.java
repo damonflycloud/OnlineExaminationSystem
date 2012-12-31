ce95f570e583212d851dd7ab93126f793386d1c0
flycloud <damonflycloud@126.com>
2012-12-31 13:01:24 +0800

    test commit

---
+++ src/com/flycloud/util/mytest.java
@@ -0,0 +1,25 @@
+package com.flycloud.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lenovo
+ * Date: 12-12-31
+ * Time: 下午12:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class mytest {
+    private int myname;
+
+    public int getMyname() {
+        return myname;
+    }
+
+    public void setMyname(int myname) {
+        this.myname = myname;
+    }
+
+    public mytest(){
+
+    }
+
+}
